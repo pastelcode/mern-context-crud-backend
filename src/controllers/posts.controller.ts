ebdc9afacf411b5ec6f523de2b2701db6d382aeb
Pastel <pastel.dev.gt@gmail.com>
2022-06-22 17:55:53 -0600

    Create controllers
    - getPosts
    - getPost
    - createPost

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,10 +1,38 @@
 import type { Handler } from 'express'
+import httpStatus from 'http-status'
+import Post from '../models/Post.js'
 
-export const getPosts: Handler = (_req, res) => res.send([])
+export const getPosts: Handler = async (_req, res) => {
+  try {
+    const posts = await Post.find()
+    return res.status(httpStatus.OK).send(posts)
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error)
+  }
+}
 
-export const getPost: Handler = (_req, res) => res.send('Getting post...')
+export const getPost: Handler = async (req, res) => {
+  const { postId } = req.params
+  try {
+    const post = await Post.findById(postId)
+    if (post === null) {
+      return res.status(httpStatus.NOT_FOUND).send()
+    }
+    return res.status(httpStatus.OK).send(post)
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error)
+  }
+}
 
-export const createPost: Handler = (_req, res) => res.send('Creating post')
+export const createPost: Handler = async (req, res) => {
+  const { title, description } = req.body
+  try {
+    const { id } = await Post.create({ title, description })
+    return res.status(httpStatus.CREATED).send(id)
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error)
+  }
+}
 
 export const updatePost: Handler = (_req, res) => res.send('Updating post...')
 
