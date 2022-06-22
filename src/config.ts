71b29b2a4c43801542b61b48ec9c8e8ce4a336f7
Pastel <pastel.dev.gt@gmail.com>
2022-06-21 20:51:50 -0600

    Read mongodb uri from env

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,8 @@
+import { config } from 'dotenv'
+
+config()
+
+export const mongoURI =
+  process.env['MONGODB_URI'] || 'mongodb://localhost/mern-context-crud'
+
 export const port = process.env['PORT'] || 4000
